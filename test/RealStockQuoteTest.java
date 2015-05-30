bf66d88c7549f9ac777c41f7f7bea5d088e97fba
Aydin Gungordu <aydin.gungordu@itera.no>
2015-05-30 02:34:34 +0200

    Fixing test

--- test/RealStockQuoteTest.java
+++ test/RealStockQuoteTest.java
@@ -2,16 +2,15 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import utils.RealFundQuote;
+import utils.FundQuote;
 
 public class RealStockQuoteTest {
 
   @Test
   public void realFundTest() throws IOException {
-    RealFundQuote fundQuote = new RealFundQuote();
+    FundQuote fundQuote = new FundQuote();
 
-    System.out.println(RealFundQuote.getFundChange(fundQuote.getFundHoldings("DNBNorgeIndex")));
- //   System.out.println(fundQuote.getFundChange("DNBNorgeIndex"));
+    System.out.println(FundQuote.getFundChange(fundQuote.getFundHoldings("DNBNorgeIndex"),null));//   System.out.println(fundQuote.getFundChange("DNBNorgeIndex"));
   }
 
 }
